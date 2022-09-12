844e71f8e239fa51e3b4c7bcf6a2e59ead03910a
+ <+>
2022-09-12 23:37:40 +0300

    done

--- Desktop/phase3-project/my-frontend/src/components/Fundi /Fundi.js
+++ Desktop/phase3-project/my-frontend/src/components/Fundi /Fundi.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React from 'react'
+// import Comment from '../Comment/Comment'
+import {useEffect, useState} from "react";
 
-const Fundi = () =>
-{
-    return (
-        <div>content regarding fundis</div>
+
+const Fundi = () => {
+  const [Fundi, setFundi] = useState([])
+
+    useEffect(()=>{
+        fetch("https://localhost/9292/contracts")
+        .then(res=>res.json())
+        .then((data) =>
+        setFundi(data)   
+        )
+    },[])
+    console.log(Fundi)
+
+    const allComments = Fundi.map((fundi)=>{
+      return( <Comment  key={fundi.id}
+        name={fundi.name}
+        location ={fundi.location}
+        body = {fundi.ratings.map(review => {
+            return review.body     
+        })}
+         user = {restaurant.reviews.map(review => {
+            return review.user.name
+
+         })}
+
+        />)
+    })
+    return(
+    <>
+    <div className="allCommnets">
+    {allComments}
+    </div>
+    </>
     )
+  
+
+
+
+  
 }
-export default Fundi
+
+export default Restuarants
